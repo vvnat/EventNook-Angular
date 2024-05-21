7e5db0d0f9e923f5f30796da6fb04ddc908655f3
vvnat <nrisuenosantiago@gmail.com>
2024-05-21 16:40:17 +0200

    formulario empezado pero está increiblemente mal

--- src/app/pages/nuevo-evento/nuevo-evento.component.ts
+++ src/app/pages/nuevo-evento/nuevo-evento.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { EventTypeForm } from '../../types/EventTypeForm';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-nuevo-evento',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ReactiveFormsModule],
   templateUrl: './nuevo-evento.component.html',
   styleUrl: './nuevo-evento.component.css'
 })
 export class NuevoEventoComponent {
+  protected eventTypes: string[] = ["Bodas y comuniones", "Cenas y comidas de empresa","Congresos, conferencias y charlas", "Conciertos"];
 
+  eventTypeForm = new FormGroup({
+    name: new FormControl('')
+  });
+
+  onEventType(): void {
+    const formValue = this.eventTypeForm.value;
+
+    const form: EventTypeForm = {
+      name: this.eventTypeForm.value.name ?? ''
+    };
+  }
 }
---
+++ src/app/types/EventTypeForm.ts
@@ -0,0 +1,3 @@
+export type EventTypeForm = {
+    name: string;
+  }
