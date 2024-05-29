d84c0cf01f18ae87b803f003389db65e5fe5b7cc
vvnat <nrisuenosantiago@gmail.com>
2024-05-29 14:14:47 +0200

    dropdown funciona!!

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,12 +4,13 @@ import { MatFormField } from '@angular/material/select';
 import { MatOption } from '@angular/material/select';
 import { CookieService } from 'ngx-cookie-service';
 import { UserService } from '../../services/user.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatSelect, MatFormField, MatOption, RouterLink],
+  imports: [MatSelect, MatFormField, MatOption, RouterLink, DropdownModule],
   providers: [CookieService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
@@ -17,6 +18,22 @@ import { RouterLink } from '@angular/router';
 export class HeaderComponent {
   cookieService: CookieService = inject(CookieService);
   userService: UserService = inject(UserService);
+
+  opciones = [ 
+    { name: "Espacios", url: "/spaces" }, 
+    { name: "Caterings", url: "/caterings" }, 
+    { name: "Músicos", url: "/musicians" }, 
+    { name: "Restaurantes", url: "/restaurants" },
+  ]; 
+
+  router: Router = inject(Router);
+
+  onChange(event: any) {
+    console.log('event :' + event);
+    console.log(event.value);
+    this.router.navigate([event.value.url]); 
+}
+
  /* user: User = JSON.parse(this.cookieService.get('user'));
 
   username:string = this.user.username || "";
