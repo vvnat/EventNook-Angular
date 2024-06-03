04e03de038591ca91384ce82cf8074a0b21daeaa
vvnat <nrisuenosantiago@gmail.com>
2024-06-03 13:07:18 +0200

    withFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,13 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideAnimations()
   ]
 };
